b20ab3861c703bbb067dc04e25dd7418e88cb0c6
Harish <harish77505550@gmail.com>
2026-05-31 20:25:14 +0530

    Update db_connect.php

--- Login/db_connect.php
+++ Login/db_connect.php
@@ -1,13 +1,18 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $conn = new mysqli(
-    "zephyr.proxy.rlwy.net",
-    "root",
-    "ChoNLqhbXxKcmIJBDluloGGheuHBabKY",
-    "railway",
-    50789
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE'),
+    getenv('MYSQLPORT')
 );
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
